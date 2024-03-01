b530db39cdd0917b6d23092887bda99150462a62
MartinMurithi <wachiramartin84@gmail.com>
2024-03-01 21:13:46 +0300

    Email confirmation

--- server/config/DB.js
+++ server/config/DB.js
@@ -3,10 +3,9 @@ require("dotenv").config();
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: 
-  "postgres",
-  host: "localhost",
-  database: "ecommerce",
+  user: process.env.User,
+  host: process.env.Host,
+  database: process.env.Database,
   password: process.env.Password,
   port: process.env.DB_PORT,
 });
