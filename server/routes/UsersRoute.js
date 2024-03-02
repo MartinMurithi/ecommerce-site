ff9694238639ffc8012245dbe29907a59f12bc71
MartinMurithi <wachiramartin84@gmail.com>
2024-03-02 03:28:16 +0300

    Hash passwords

--- server/routes/UsersRoute.js
+++ server/routes/UsersRoute.js
@@ -1,9 +1,10 @@
-const { getAllUsers, registerUser, verifyEmail } = require("../controllers/UserController");
+const { getAllUsers, registerUser, verifyEmail, logIn } = require("../controllers/UserController");
 
 const userRouter = require("express").Router();
 
 userRouter.get("/users", getAllUsers);
 userRouter.post("/register-user", registerUser);
 userRouter.get("/verify-email/:email_token", verifyEmail);
+userRouter.post("/login", logIn);
 
 module.exports = userRouter;
--- server/utils/HashPassword.js
+++ server/utils/HashPassword.js
@@ -1,8 +1,23 @@
 const bcrypt = require("bcrypt");
 const SALT_ROUNDS = 10;
-async function HashPassword(password){
+
+async function HashPassword(password) {
+  try {
     const hashedPwd = await bcrypt.hash(password, SALT_ROUNDS);
     return hashedPwd;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-module.exports = HashPassword
+async function ComparePassword(password, hash) {
+  try {
+    const isPasswordMatch = await bcrypt.compare(password, hash);
+    console.log("Password match : " + isPasswordMatch);
+    return isPasswordMatch;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+module.exports = { HashPassword, ComparePassword };
